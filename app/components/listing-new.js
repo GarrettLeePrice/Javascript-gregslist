11e5453243645c9608b79a952b867a31a1aceb0d
Garrett Price and HK Kahng <garrett@>
2016-10-31 10:21:16 -0700

    add new listing form

---
+++ app/components/listing-new.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  displayListingForm: false,
+  actions: {
+    showListingForm() {
+      this.set('displayListingForm', true);
+    },
+    saveListing() {
+      var params = {
+        seller: this.get('seller') ? this.get('seller') : "",
+        description: this.get('description') ? this.get('description') : "",
+        location: this.get('location') ? this.get('location') : "",
+        note: this.get('note') ? this.get('note') : "",
+        price: this.get('price') ? parseInt(this.get('price')) : 0,
+        category: this.get('category')
+      };
+      this.sendAction('saveListing', params);
+      this.set('displayListingForm', false);
+    }
+  }
+});
--- app/routes/category.js
+++ app/routes/category.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params) {
+   return this.store.findRecord('category', params.category_id);
+  },
+  actions: {
+    saveListing(params) {
+      var newListing = this.store.createRecord('listing', params);
+      newListing.save();
+      this.transitionTo('index');
+    }
+  }
 });
